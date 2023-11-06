c02e9c29c083abb0e501197f561f9a92c6f4694a
DungNT0 <91143508+DungNT0@users.noreply.github.com>
2023-11-06 10:32:09 +0700

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
 import CreateTasksBar from './components/CreateTasks_Bar/CreateTasksBar';
-import TasksList from './components/Tasks_List/TasksList';
 import { BrowserRouter } from "react-router-dom";
-import $ from 'jquery';
 import React from 'react';
 
 function App() {
@@ -12,77 +10,9 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <CreateTasksBar />
-        <TasksList />
       </BrowserRouter>
     </div>
   );
 }
-$(document).ready(function () {
-  console.log("jQuery loaded");
-});
 
-$(function () {
-  // jQuery - $ string selector
-  var myOutput2 = $('#my-output-2').html();
-  console.log(myOutput2);
-
-  // jQuery - $ string selector
-  var myOutput = $('#my-output').html();
-  console.log(myOutput);
-
-  // capture click event on button one
-  $('#my-button').click(function () {
-
-    var myInput2 = $('#my-input-2').val();
-    console.log(myInput2);
-
-    $('#my-output').html(myInput2);
-
-    var myInput = $('#my-input').val();
-    console.log(myInput);
-
-    // ammend content on ID
-    $('#my-output-2').html(myInput);
-    $('#my-output').html(myInput);
-  });
-
-  // capture click from button
-  var count = 1;
-  $('#my-button').on('click', function () {
-    // add new list item to ul
-    var myInput2 = $('#my-input-2').val();
-    console.log(myInput2);
-
-    var myInput = $('#my-input').val();
-    console.log(myInput);
-
-    // ammend content on ID
-    $('#nav-menu').append('<div> <li>' + myInput2 + " - " + myInput + '</li>' +
-      '<button class="complete_btn" id="complete_button' + count + '"></button>'
-      + '<button class="delete_btn" id="delete_button' + count + '"></button> </div>');
-
-    count++;
-    $(".delete_btn").on('click mouseclick', function () {
-
-      // prevent default click
-      // event.preventDefault();
-
-      console.log($(this).html());
-
-      this.parentNode.remove();
-    });
-
-    $(".complete_btn").on('click mouseclick', function () {
-
-      // prevent default click
-      // event.preventDefault();
-
-      console.log($(this).html());
-
-      $('li').css('background-color', '#1FF627');
-      $('.delete_btn').remove();
-    });
-  });
-
-});
 export default App;
--- my-app/src/components/CreateTasks_Bar/CreateTasksBar.js
+++ my-app/src/components/CreateTasks_Bar/CreateTasksBar.js
@@ -1,7 +1,49 @@
 import React from "react";
+import $ from 'jquery';
 import "./CreateTasksBar.css";
 
 const CreateTasksBar = () => {
+    const handleCreate = () => {
+        $(function () {
+            // capture click from button
+            $('#my-button').on('click', function () {
+                // add new list item to ul
+                var myInput2 = $('#my-input-2').val();
+                console.log(myInput2);
+
+                var myInput = $('#my-input').val();
+                console.log(myInput);
+
+                // ammend content on ID
+                $('#nav-menu').append('<div> <li>' + myInput2 + " - " + myInput + '</li>' +
+                    '<button class="complete_btn" id="complete_button"></button>'
+                    + '<button class="delete_btn" id="delete_button"></button> </div>');
+
+                $(".delete_btn").on('click', function () {
+
+                    // prevent default click
+                    // event.preventDefault();
+
+                    console.log($(this).html());
+
+                    this.parentNode.remove();
+                });
+
+                $(".complete_btn").on('click', function () {
+
+                    // prevent default click
+                    // event.preventDefault();
+
+                    console.log($(this).html());
+
+                    $('li').css('background-color', '#1FF627');
+                    $('.delete_btn').remove();
+                });
+            });
+
+        });
+    }
+
     return (
         <div className="CreateTasksBar">
             <div id="my_title">TO DO LIST</div>
@@ -11,7 +53,14 @@ const CreateTasksBar = () => {
 
                 <input type="text" id="my-input" />
 
-                <button id="my-button">CREATE</button>
+                <button id="my-button" onClick={handleCreate}>CREATE</button>
+            </div>
+
+            <div className="container">
+                <div id="tasks_header">TASKS</div>
+                <ul id="nav-menu">
+
+                </ul>
             </div>
         </div>
     );

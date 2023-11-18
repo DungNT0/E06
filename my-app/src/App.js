9bb905508f15b3a34aedffb8e8f44878ce7062e6
DungNT0 <91143508+DungNT0@users.noreply.github.com>
2023-11-18 16:58:00 +0700

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
-// import TasksBar from './components/CreateTasks_Bar/TasksBar';
-// import TasksList from './components/TasksList/TasksList';
+import TasksBar from './components/CreateTasks_Bar/TasksBar';
+import TasksList from './components/TasksList/TasksList';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from 'react';
 import LoginScreen from './components/LoginScreen/LoginScreen';
@@ -14,10 +14,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" exact element={<LoginScreen />} />
-
+          <Route path="/todo" exact element={<> <TasksBar /><TasksList /> </>} />
         </Routes>
-        {/* <TasksBar />
-        <TasksList /> */}
       </BrowserRouter>
     </div>
   );
